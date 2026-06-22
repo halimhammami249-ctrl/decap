811274d5798f672267b7eaadfc39675ac6ccaeda
halim hammami <halim.hammami249@gmail.com>
2026-06-22 14:42:11 +0100

    Fix JSON products structure for Decap 232gùmùfCMS

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-fetch('products.json')
+fetch('/products.json')
   .then((response) => response.json())
-  .then((products) => {
+  .then((data) => {
     const container = document.getElementById('products');
+
+    container.innerHTML = '';
+
+    // CASE 1: Decap "list field" format (your setup)
+    const products = data.products || data;
 
     products.forEach((product) => {
       container.innerHTML += `
         <div>
           <h2>${product.name}</h2>
-          <img src="${product.image}" width="150">
-          <p>${product.price}$</p>
+          <img src="${product.image}" width="150" />
+          <p>${product.price} $</p>
           <p>${product.description}</p>
         </div>
       `;
     });
+  })
+  .catch((err) => {
+    console.error('Error loading products:', err);
   });
